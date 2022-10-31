6824d8f715d6846914765a1e9be98aab3b59b981
yuliaMasliak <1985kozak@gmail.com>
2022-10-31 15:21:58 +0200

    feat: add js to nav links

--- pages/main/script.js
+++ pages/main/script.js
@@ -1,10 +1,11 @@
-let btnContainer = document.getElementById("active-links");
-let btns = btnContainer.getElementsByClassName("active-link");
+let header = document.getElementById("active-links");
+
+let btns = header.getElementsByClassName("active-link");
 
 for (let i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
-    let current = document.getElementsByClassName("active-nav");
-    current[0].className = current[0].className.replace(" active-nav", "");
-    this.className += " active-nav";
+  let current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
   });
 }
