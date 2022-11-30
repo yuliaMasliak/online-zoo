14be530140dac3244ea3089c678cb2c366e5e26d
Yulia Masliak <110036110+yuliaMasliak@users.noreply.github.com>
2022-11-30 19:24:24 +0200

    feat: add slider to gallary

--- pages/main/script.js
+++ pages/main/script.js
@@ -70,3 +70,41 @@ function openMenu(event){
         }
       };
 
+      //Slider gallery animal pictures
+//       let slides = document.querySelectorAll(".gallery-blocks");
+// let rightArrow = document.querySelector(".right-arrow");
+// console.log(rightArrow)
+// console.log(slides)
+// rightArrow.addEventListener("click", slideGallery);
+// function slideGallery(event){
+//   slides[0].classList.toggle("two");
+//   }
+// console.log(slides[0]);
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("gallery");
+
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex-1].style.display = "grid";
+
+}
+
