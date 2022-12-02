95ee68d7dd233a9e4e6c94a58a9eb676ab7f0df3
Yulia Masliak <110036110+yuliaMasliak@users.noreply.github.com>
2022-12-02 12:10:25 +0200

    feat: add scroll function to feedbacks

--- pages/main/script.js
+++ pages/main/script.js
@@ -100,16 +100,132 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "grid";
 }
 
+
+
 //Feedbacks scroll
 var range = document.getElementById("range");
 var newValue = range.value;
+let child1 = document.querySelector(`#first`);
+let child2 = document.querySelector(`#second`);
+let child3 = document.querySelector(`#third`);
+let child4 = document.querySelector(`#fourth`);
+let child5 = document.querySelector(`#fifth`);
+let child6 = document.querySelector(`#sixth`);
+let child7 = document.querySelector(`#seventh`);
+let child8 = document.querySelector(`#eight`);
+let child9 = document.querySelector(`#nineth`);
+let child10 = document.querySelector(`#tenth`);
+let child11 = document.querySelector(`#eleventh`);
 
 range.addEventListener("input", rangeValue);
 function rangeValue(event){
-
-  let child = document.querySelector(`#carousel :nth-of-type(${range.value})`);
-      child.classList.toggle("pos-1");
+if(range.value == 1){
+  child1.style.display = "block";
+  child2.style.display = "block";
+  child3.style.display = "block";
+  child4.style.display = "block";
+  child5.style.display = "none";
+  child6.style.display = "none";
+  child7.style.display = "none";
+  child8.style.display = "none";
+  child9.style.display = "none";
+  child10.style.display = "none";
+  child11.style.display = "none";
 }
+if(range.value == 2){
+  child1.style.display = "none";
+  child2.style.display = "block";
+  child3.style.display = "block";
+  child4.style.display = "block";
+  child5.style.display = "block";
+  child6.style.display = "none";
+  child7.style.display = "none";
+  child8.style.display = "none";
+  child9.style.display = "none";
+  child10.style.display = "none";
+  child11.style.display = "none";
+}
+if(range.value == 3){
+  child1.style.display = "none";
+  child2.style.display = "none";
+  child3.style.display = "block";
+  child4.style.display = "block";
+  child5.style.display = "block";
+  child6.style.display = "block";
+  child7.style.display = "none";
+  child8.style.display = "none";
+  child9.style.display = "none";
+  child10.style.display = "none";
+  child11.style.display = "none";
+}
+if(range.value == 4){
+  child1.style.display = "none";
+  child2.style.display = "none";
+  child3.style.display = "none";
+  child4.style.display = "block";
+  child5.style.display = "block";
+  child6.style.display = "block";
+  child7.style.display = "block";
+  child8.style.display = "none";
+  child9.style.display = "none";
+  child10.style.display = "none";
+  child11.style.display = "none";
+}
+if(range.value == 5){
+  child1.style.display = "none";
+  child2.style.display = "none";
+  child3.style.display = "none";
+  child4.style.display = "none";
+  child5.style.display = "block";
+  child6.style.display = "block";
+  child7.style.display = "block";
+  child8.style.display = "block";
+  child9.style.display = "none";
+  child10.style.display = "none";
+  child11.style.display = "none";
+}
+if(range.value == 6){
+  child1.style.display = "none";
+  child2.style.display = "none";
+  child3.style.display = "none";
+  child4.style.display = "none";
+  child5.style.display = "none";
+  child6.style.display = "block";
+  child7.style.display = "block";
+  child8.style.display = "block";
+  child9.style.display = "block";
+  child10.style.display = "none";
+  child11.style.display = "none";
+}
+if(range.value == 7){
+  child1.style.display = "none";
+  child2.style.display = "none";
+  child3.style.display = "none";
+  child4.style.display = "none";
+  child5.style.display = "none";
+  child6.style.display = "none";
+  child7.style.display = "block";
+  child8.style.display = "block";
+  child9.style.display = "block";
+  child10.style.display = "block";
+  child11.style.display = "none";
+}
+if(range.value == 8){
+  child1.style.display = "none";
+  child2.style.display = "none";
+  child3.style.display = "none";
+  child4.style.display = "none";
+  child5.style.display = "none";
+  child6.style.display = "none";
+  child7.style.display = "none";
+  child8.style.display = "block";
+  child9.style.display = "block";
+  child10.style.display = "block";
+  child11.style.display = "block";
+}
+}
+console.log(range.value);
+console.log(child11);
 
 //Testimonial popup
 
